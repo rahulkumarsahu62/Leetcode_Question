407eb2c4a14a91926a978651fb2a8024cd7658bf
Rahul Kumar Sahu <rahulkrsahu62@gmail.com>
2026-05-11 20:55:30 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (92.93%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        int xor1 = 0;
+        int xor2 = 0;
+
+        for(int i = 0; i<n; i++){
+
+            xor1 = xor1^(i+1);
+            xor2 = xor2^nums[i];
+        }
+        return xor1 ^ xor2;
+
+    }
+};
