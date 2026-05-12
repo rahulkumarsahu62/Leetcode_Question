300fa360c343f9695a464652077020bc1d98b515
Rahul Kumar Sahu <rahulkrsahu62@gmail.com>
2026-05-12 08:48:52 +0530

    Time: 0 ms (100.00%) | Memory: 24.9 MB (98.12%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> freq(n+1,0);
+        long long  n = nums.size();
+
+        long long expectedSum = n *(n + 1)/2;
+        long long expectedsquareSum = n *(n+1)*(2*n+1)/6;
+
+        long long actualSum = 0;
+        long long actualsquareSum = 0;
 
         for(int x : nums){
-            freq[x]++;
+            actualSum += x;
+            actualsquareSum += 1LL * x *x;
         }
-        int duplicate = -1;
-        int missing = -1;
+        long long diff1 = actualSum - expectedSum;  // x-y
+        long long diff2 = actualsquareSum - expectedsquareSum; // x^2 -y^2
 
-        for(int i = 1; i<=n; i++){
-            if(freq[i] == 2){
-                duplicate = i;
-            }
-            else if (freq[i] == 0){
-                missing = i;
-            }
-        }
-        return {duplicate,missing};
+        long long sumXY = diff2 / diff1;  // x+y = x^2 -Y^2 / x-y
+
+        long long duplicate = (diff1 + sumXY)/2;   
+        long long missing = sumXY - duplicate;
+
+        return {(int)duplicate,(int)missing};
     }
 };
