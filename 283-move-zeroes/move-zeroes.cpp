ea35344de0da28244a3ff69f9a07efdea7b15e85
Rahul Kumar Sahu <rahulkrsahu62@gmail.com>
2026-08-05 17:13:25 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (83.33%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,7 +4,7 @@ public:
        int n = nums.size();
        if ( n == 0) return;
        int i = 0;
-       for(int j = 0; j<n; j++){
+       for(int j =0; j<n; j++){
         if(nums[j] != 0){
             swap(nums[i],nums[j]);
             i++;
