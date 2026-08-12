0037bbe80267c86f6bc12353c180197e0a0630c8
Rahul Kumar Sahu <rahulkrsahu62@gmail.com>
2026-08-12 17:22:50 +0530

    Time: 340 ms (10.70%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT ans.machine_id,ROUND(sum(ans.diff)/count(ans.process_id),3) processing_time 
+FROM(
+SELECT a1.machine_id,a1.process_id,(a2.timestamp-a1.timestamp)diff FROM Activity a1 JOIN Activity a2
+ON (
+a1.machine_id = a2.machine_id AND
+a1.process_id = a2.process_id AND
+a1.activity_type = 'start'AND
+a2.activity_type = 'end'
+)
+) ans
+GROUP BY ans.machine_id;
