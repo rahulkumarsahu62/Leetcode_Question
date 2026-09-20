82ab199591c3e2f1fc58e9e59a0aec582747fab6
Rahul Kumar Sahu <rahulkrsahu62@gmail.com>
2026-09-20 10:16:53 +0530

    Time: 479 ms (70.83%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3803-find-products-with-valid-serial-numbers/find-products-with-valid-serial-numbers.sql
@@ -0,0 +1,5 @@
+SELECT product_id, product_name, description
+FROM products
+WHERE description COLLATE utf8mb3_bin
+REGEXP '(^|[^A-Za-z0-9])SN[0-9]{4}-[0-9]{4}([^A-Za-z0-9]|$)'
+ORDER BY product_id;
